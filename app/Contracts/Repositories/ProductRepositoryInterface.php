3c8db7eba4d7c3ca49f47c12a276d4dd452e9be8
Henry Marsch <marschiee@gmail.com>
2020-08-05 20:10:16 +0200

    Updated to Laravel version 7

--- app/Contracts/Repositories/ProductRepositoryInterface.php
+++ app/Contracts/Repositories/ProductRepositoryInterface.php
@@ -4,7 +4,7 @@ namespace App\Contracts\Repositories;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 
-interface ProductRepositoryInterface extends  EloquentRepositoryInterface
+interface ProductRepositoryInterface extends EloquentRepositoryInterface
 {
     /**
      * @param string $name
